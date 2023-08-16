7c2fd6d02b2d3fdd52b1e27a26ab1c491ac385dd
dannycornelio <licdannycornelio@gmail.com>
2023-08-16 09:52:54 -0600

    segunda config del index de login

--- views/login/index.php
+++ views/login/index.php
@@ -1,1 +1,23 @@
-<h1>login</h1>
+<h2 class="text-center mb-4 text-primary">Inicio de sesion</h2>
+<div class="row justify-content-center">
+    <form class="col-lg-4 border rouded p-3">
+        <div class="row mb-3">
+            <div class="col">
+                <label for="usu_catologo" class="form-label">Catalogo</label>
+                <input type="number" class="form-control" id="usu_catalogo">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col">
+                <label for="usu_password" class="form-label">Contraseña</label>
+                <input type="password" class="form-control" id="usu_password">
+            </div>
+        </div>
+        <div class="d-grid">
+            <button class="btn btn-primary" type="submit">Iniciar sesion</button>
+        </div>
+    </form>
+    <div class="mt-3">
+        <p class="mb-0 text-center">NO tiene cuneta?<a href="/login_prueba/registro" class="text-primary fw-bold ms-2">Registrarse</a></p>
+    </div>
+</div>
